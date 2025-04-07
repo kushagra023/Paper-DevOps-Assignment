c6faaeaaaa6175f5b5e4dbda2871e0fb5a745063
Kushagra Agarwal <138867347+kushagra023@users.noreply.github.com>
2025-04-07 12:43:57 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,10 +11,13 @@ variable "key_name" {
   type        = string
 }
 
-variable "public_key_path" {
-  description = "C:/Users/akush/Downloads/DevOpsKey.pem"
-  type        = string
+variable "public_key" {}
+
+resource "aws_key_pair" "dev_key" {
+  key_name   = "dev_key"
+  public_key = var.public_key
 }
+
 
 variable "ami_id" {
   description = "Ubuntu 22.04 LTS in ap-south-1"
